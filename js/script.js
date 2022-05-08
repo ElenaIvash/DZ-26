26c42538ca2909d0c2714865179b3d93e69f39ca
Elena <77905432@mail.ru>
2022-05-08 15:44:31 +0300

    login form implementation

---
+++ js/script.js
@@ -0,0 +1,22 @@
+/*function getFactorial(a) {
+return (a != 1) ? a * getFactorial(a - 1) : 1;
+}
+alert(getFactorial(3));*/
+
+
+/*function pow(x, n) {
+    let result = 1;
+        for (let i = 0; i < n; i++) {
+      result *= x;
+    }
+    return result;
+  }
+  alert( pow(2, 3) );*/
+  
+
+  /*function sum(a, b) {
+    if(b == 0)
+        return a;
+    return sum(a + 1, b - 1);
+}
+alert(sum(8, 3));*/
